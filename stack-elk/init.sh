c158d45e73e3add435570685d45fb7ab2a2b11a8
root <root@localhost.localdomain>
2018-05-16 17:04:53 +0200

    maj stack

---
+++ stack-elk/init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+cmd=$1
+
+#Modif permissions
+sudo chown root:root metricbeat/metricbeat.yml
+sudo chown root:root -R metricbeat/modules.d/*
+sudo chmod go-w -R metricbeat/modules.d/*
+
+echo "Modification de permissions OK"
+echo "Lancer la stack avec: sudo docker-compose up"

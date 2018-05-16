8f313cfd378f6161c8a2f45be200920b911dd4f9
corentin <corentin@localhost.localdomain>
2018-05-16 22:16:36 +0200

    modif init

--- stack-elk/init.sh
+++ stack-elk/init.sh
@@ -5,6 +5,7 @@ cmd=$1
 #Modif permissions
 sudo chown root:root metricbeat/metricbeat.yml
 sudo chown root:root -R metricbeat/modules.d/*
+sudo chmod go-w -R metricbeat/metricbeat.yml
 sudo chmod go-w -R metricbeat/modules.d/*
 
 echo "Modification de permissions OK"

a6d70bf7cb494edd3a0c1ae1a3e877cb0729f8b2
corentin <corentin@localhost.localdomain>
2018-05-17 17:10:21 +0200

    modif logspout

--- stack-elk/logspout/build.sh
+++ stack-elk/logspout/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+export http_proxy=http://193.56.47.8:8080
+export https_proxy=http://193.56.47.8:8080
 set -e
-apk add --update go build-base git mercurial ca-certificates
+apk add --update go git mercurial build-base ca-certificates
 mkdir -p /go/src/github.com/gliderlabs
 cp -r /src /go/src/github.com/gliderlabs/logspout
 cd /go/src/github.com/gliderlabs/logspout
@@ -8,7 +10,8 @@ export GOPATH=/go
 go get
 go build -ldflags "-X main.Version=$1" -o /bin/logspout
 apk del go git mercurial build-base
-rm -rf /go /var/cache/apk/* /root/.glide
+rm -rf /go
+rm -rf /var/cache/apk/*
 
 # backwards compatibility
 ln -fs /tmp/docker.sock /var/run/docker.sock
